2acc950d7277162b666ffb286f979718f0fb6ff4
vidya <vidyaranielchuri@gmail.com>
2025-09-02 13:06:26 +0530

    first commit

---
+++ objects.py
@@ -0,0 +1,8 @@
+class objects:
+    def __init__(self):
+        self.colour=None
+        self._shape="Cirlce"
+    def display(self,s):
+        self._shape=s
+obj=objects()
+print(obj.objects_shape)
